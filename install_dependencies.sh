dd313674f84cfef513b22b7ec6992bd6242e860c
mromanello <matteo.romanello@gmail.com>
2017-01-28 15:20:20 +0100

    ...still getting travis to work...

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,13 +2,36 @@
 # (this script should be ran as root/with sudo!)
 #
 # INSTALL CRF++
-wget "https://googledrive.com/host/0B4y35FiV1wh7fngteFhHQUN2Y1B5eUJBNHZUemJYQV9VWlBUb3JlX0xBdWVZTWtSbVBneU0/CRF++-0.58.tar.gz" -O CRF++.tar.gz
-tar -xzf CRF++.tar.gz
-chmod 777 -R CRF++-0.58/ 
-cd CRF++-0.58/
+git clone https://github.com/taku910/crfpp.git
+cd crfpp/
 ./configure
-make
+sed -i '/#include "winmain.h"/d' crf_test.cpp
+sed -i '/#include "winmain.h"/d' crf_learn.cpp
 make install
 make clean
 ldconfig
-rm ../CRF++.tar.gz
+rm -fr ../crfpp
+# INSTALL TREETAGGER
+mkdir tree-tagger
+cd tree-tagger/
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tree-tagger-linux-3.2.tar.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tagger-scripts.tar.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/install-tagger.sh
+# parameter files for tagger
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/dutch-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/french-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/german-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/italian-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/latin-par-linux-3.2.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/portuguese-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish-par-linux-3.2-utf8.bin.gz
+# parameter files for chunker
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-chunker-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/french-chunker-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/german-chunker-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish-chunker-par-linux-3.2-utf8.bin.gz
+# let's run the installation script
+chmod +x install-tagger.sh 
+./install-tagger.sh
+rm *.gz
