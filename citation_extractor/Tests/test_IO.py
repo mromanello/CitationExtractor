ed8ac52bddcb677beb05466ebbeefa19bf3f8337
Matteo Filipponi <matteo.filipponi@epfl.ch>
2017-02-21 15:12:04 +0100

    added all

--- citation_extractor/Tests/test_IO.py
+++ citation_extractor/Tests/test_IO.py
@@ -11,6 +11,7 @@ from citation_extractor.Utils.IO import annotations2references
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 @fixture
 def knowledge_base():
 	"""
@@ -19,6 +20,8 @@ def knowledge_base():
 	kb = KB2("/media/romanell_files/tmp/virtuoso.ini")
 	logger.info("The KnowledgeBase %s was initialised"%kb)
 	return kb
+
+
 def test_annotations2references(knowledge_base):
 	datadir = ('citation_extractor','data/aph_corpus/goldset/ann/')
 	dir = pkg_resources.resource_filename(*datadir)
