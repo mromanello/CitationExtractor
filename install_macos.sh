ee081727ca528055e7f390b7bae4ff97f0f96e6f
Matteo Filipponi <matteo.filipponi@epfl.ch>
2017-02-23 14:38:31 +0100

    small modif to install_macos script

--- install_macos.sh
+++ install_macos.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-# (this script should be ran with sudo -H)
 
+# (this script should be ran with sudo -H)
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root, try with sudo -H" 1>&2
+   exit 1
+fi
+
+# Set the paths for the project and its dependencies
 project_dir=$(pwd)
 dependencies_dir=${project_dir}/..
+echo "> CitationExtractor will be installed in: $project_dir"
+echo "> CitationExtractor dependencies will be installed in: $dependencies_dir"
 
+# Probably not necessary (?)
 export C_INCLUDE_PATH=/usr/local/include/:${C_INCLUDE_PATH}
 export CPLUS_INCLUDE_PATH=/usr/local/include/:${CPLUS_INCLUDE_PATH}
 
 # INSTALL CRF++
+echo "> Installing crfpp"
 cd $dependencies_dir
 git clone https://github.com/taku910/crfpp.git
 cd crfpp/
@@ -16,11 +26,11 @@ sed -i'' -e '/#include "winmain.h"/d' crf_test.cpp
 sed -i'' -e '/#include "winmain.h"/d' crf_learn.cpp
 make install
 make clean
-#ldconfig
 chmod -R 777 ../crfpp
 pip install -e python
 
 # INSTALL TREETAGGER
+echo "> Installing tree-tagger"
 cd $dependencies_dir
 mkdir tree-tagger
 cd tree-tagger/
@@ -48,19 +58,22 @@ rm *.gz
 
 
 # INSTALL HUCIT KB
+echo "> Installing hucit_kb"
 cd $dependencies_dir
 git clone https://github.com/mromanello/hucit_kb.git
 cd hucit_kb
-pip install -r requirements.txt
+pip install -U -r requirements.txt
 pip install .
 
 # INSTALL CITATION_PARSER
+echo "> Installing CitationParser"
 cd $dependencies_dir
 git clone https://github.com/mromanello/CitationParser.git
 cd CitationParser
 python setup.py install
 
 # INSTALL PROJECT (CITATION_EXTRACTOR)
+echo "> Installing CitationExtractor"
 cd $project_dir
 pip install -e lib
 pip install -U -r requirements.txt
