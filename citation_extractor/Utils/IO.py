efde8927c6e3b955c2c19051f53141a06d208e13
mromanello <matteo.romanello@gmail.com>
2017-05-25 19:16:31 +0200

    removing old APh data

--- citation_extractor/Utils/IO.py
+++ citation_extractor/Utils/IO.py
@@ -11,6 +11,7 @@ from pyCTS import CTS_URN
 
 global logger
 logger = logging.getLogger(__name__)
+
 def read_ann_file(fileid,ann_dir):
     """
     TODO
@@ -53,6 +54,7 @@ def read_ann_file(fileid,ann_dir):
             entity = entities[rel_id[0]]
             res_annotations.append([rel_id[0],entity[1],urn])
     return entities, relations, res_annotations
+
 def read_ann_file_new(fileid, ann_dir, suffix="-doc-1.ann"):
     """
     TODO
@@ -93,6 +95,7 @@ def read_ann_file_new(fileid, ann_dir, suffix="-doc-1.ann"):
                                 ,"arguments":(arg1.split(":")[1], arg2.split(":")[1])
                                 ,"relation_type":rel_type}
     return entities, relations, annotations
+
 def annotations2references(doc_id, directory, kb):
     """
     Read annotations from a brat stand-off file (.ann).
@@ -213,6 +216,7 @@ def annotations2references(doc_id, directory, kb):
             logger.error("The annotations %s raised an error: %s"%(annotation,e))
     logger.info("Read %i annotations from file %s%s"%(len(annotations), directory, doc_id))
     return annotations
+
 def init_logger(log_file=None, loglevel=logging.DEBUG):
     """
     Initialise the logger
@@ -234,10 +238,12 @@ def init_logger(log_file=None, loglevel=logging.DEBUG):
         logger.addHandler(ch)
         logger.info("Logger initialised")
     return logger
+
 def count_tokens(instances):
     """
     """
     return sum([1 for instance in instances for token in instance])
+
 def write_iob_file(instances,dest_file):
     to_write = "\n\n".join(["\n".join(["\t".join(token) for token in instance]) for instance in instances])
     try:
@@ -248,6 +254,7 @@ def write_iob_file(instances,dest_file):
         return True
     except Exception, e:
         raise e
+
 def file_to_instances(inp_file):
     """
     Reads a IOB file a converts it into a list of instances.
@@ -270,6 +277,7 @@ def file_to_instances(inp_file):
         if(inst and len(inst)>1):
             out.append(inst)
     return out
+
 def read_instances(inp_text):
     out=[]
     comment=re.compile(r'#.*?')
@@ -281,59 +289,7 @@ def read_instances(inp_text):
         if(inst):
             out.append(inst)
     return out
-def out_html(out):
-    """docstring for out_xml"""
-    import libxml2,libxslt
-    from citation_extractor import core
-    xsl_path="%s/%s/%s"%(core.determine_path(),"data","reply2html.xsl")
-    styledoc = libxml2.parseFile(xsl_path)
-    logger=logging.getLogger("CREX.IO")
-    logger.info("Using stylesheet \"%s\""%xsl_path)
-    style = libxslt.parseStylesheetDoc(styledoc)
-    doc = libxml2.parseDoc(out.encode("utf-8"))
-    result = style.applyStylesheet(doc, None)
-    return style.saveResultToString(result)
-def verbose_to_XML(instances):
-    """docstring for verbose_to_XML"""
-    out = mdom.Document()
-    root = out.createElement('reply')
-    root.setAttribute("service","crefex")
-    root.setAttribute("version",citation_extractor.__version__)
-    for inst in instances:  
-        ins = out.createElement('instance')
-        for t in inst:
-            #print type(t['token'])
-            try:
-                x = unicode(t['token'],"UTF-8")
-            except Exception, e:
-                print t['token']
-                raise e
-            
-            text = out.createTextNode(x)
-            tok = out.createElement('token')
-            tok.setAttribute("label",t['label'])
-            tok.setAttribute("id",str(t['id']))
-            tok.setAttribute("value",x)
-            features = out.createElement('features')
-            features.appendChild(out.createTextNode(unicode(", ".join(t['features']),"UTF-8")))
-            #features.appendChild(out.createTextNode(", ".join(t['features'])))
-            probs = out.createElement('tags')
-            for tag in t['probs'].keys():
-                prob = out.createElement('tag')
-                prob.setAttribute("label",tag)
-                prob.setAttribute("alpha",str(t['probs'][tag]['alpha']))
-                prob.setAttribute("prob",str(t['probs'][tag]['prob']))
-                prob.setAttribute("beta",str(t['probs'][tag]['beta']))
-                out.createTextNode(tag)
-                probs.appendChild(prob)
-            tok.appendChild(probs)
-            tok.appendChild(features)
-            tok.appendChild(text)
-            tok.appendChild(out.createTextNode(" "))
-            ins.appendChild(tok)
-        root.appendChild(ins)
-    out.appendChild(root)
-    return out.toprettyxml(encoding="UTF-8")
+
 def read_IOB_file(file):
     # instances is a list of lists
     instances=[]
@@ -350,19 +306,23 @@ def read_IOB_file(file):
         if(instance):
             instances.append(instance)
     return instances
+
 def token_to_string(tok_dict):
     tmp = []
     for k in sorted(tok_dict.keys()):
         tmp.append(tok_dict[k])
     return tmp
+
 def instance_to_string(inst):
     out = []
     for fs in inst:
         tmp = token_to_string(fs)
         out.append("\t".join(tmp))
     return out
+
 def instance_to_IOB(instance):
     pass
+
 def instance_contains_label(instance,labels=["O"]):
     """
     TODO: 
@@ -373,6 +333,7 @@ def instance_contains_label(instance,labels=["O"]):
         return False
     else:
         return True
+
 def token_tostring(token):
     string=""
     for count,t in enumerate(token):
@@ -381,6 +342,7 @@ def token_tostring(token):
             else:
                 string+="%s"%t
     return string
+
 def filter_IOB(instances,tag_name):
     """docstring for filter_IOB"""
     out=[]
@@ -409,6 +371,7 @@ def filter_IOB(instances,tag_name):
     for r in out:
         res.append(' '.join(r)) 
     return res
+
 def instance_tostring(instance):
     string=""
     for count,t in enumerate(instance):
@@ -417,6 +380,7 @@ def instance_tostring(instance):
             else:
                 string+="%s"%t[0]
     return string
+
 def result_to_string(result):
     """
     Tranform the result to a string.
@@ -427,6 +391,7 @@ def result_to_string(result):
         if(i<len(result)-1):
             out+=" "
     return out
+
 def eval_results_to_HTML(results,labels=[]):
     """
     Tranform the result to a string.
@@ -453,6 +418,7 @@ def eval_results_to_HTML(results,labels=[]):
         out+="</div>"
     out+="</body></html>"
     return out
+
 def results_to_HTML(results,labels=[]):
     """
     Tranform the result to a string.
@@ -464,76 +430,7 @@ def results_to_HTML(results,labels=[]):
     out+="</div></body>"
     #out+="</html>"
     return out
-def parse_jstordfr_XML(inp):
-    """
-    Describe what the function does.
-    """
-    out=[]
-    xml_exp=re.compile(r'(?:<citation>)(.*?)(?:</citation>)')
-    xml_exp=re.compile(r'(?:<reference)(?:.*?>)(.*?)(?:</reference>)')
-    cdata_exp=re.compile(r'(?:<!\[CDATA\[)(.*?)(\]\]>)')
-    if(xml_exp.search(inp) is not None):
-        out=xml_exp.findall(inp)
-        #for item in res2:
-        #   res1=cdata_exp.match(item)
-        #   out.append(res1.groups()[0])
-    return out
-def parse_dfr_keyword_xml(inp):
-    """docstring for fname"""
-    out=[]
-    xml_exp=re.compile(r'(?:<keyterm frequency=\")(.*?)(?:\" >)(.*?)(?:</keyterm>)')
-    if(xml_exp.search(inp) is not None):
-        res2=xml_exp.findall(inp)
-        out = res2
-    return out
-def parse_dfr_wordcount_xml(inp):
-    """docstring for fname"""
-    out=[]
-    xml_exp=re.compile(r'(?:<wordcount frequency=\")(.*?)(?:\" >)(.*?)(?:</wordcount>)')
-    if(xml_exp.search(inp) is not None):
-        res2=xml_exp.findall(inp)
-        out = res2
-    return out
-def parse_one_p_line(inp):
-    """
-    Describe what the function does.
-    """
-    return [line for line in inp.split("\n")]
-def prepare_for_tagging(file_name,inp="jstor/xml"):
-    """
-    """
-    inp=open(file_name).read()
-    prolog="""# File generated from %s
-    # The tag to be used to mark up Canonical References are: B-CRF, I-CRF and O (according to the
-    # classical IOB format for NER)
-    """%(file_name)
-    out=""
-    out+=prolog
-    raw = None
-    if(inp=="jstor/xml"):
-        raw = parse_jstordfr_XML(inp)
-    else:
-        raw = parse_one_p_line(inp)
-    for i in raw:
-        out+=("\n# Original line: %s\n"%i)
-        for t in i.split(' '):
-            out+="%s\tO\n"%t
-        out+="\n"
-    return out
-def read_jstor_data(dir):   
-    """
-    Returns a list of strings being the absolute paths to XML files in the dataset
-    """
-    import os,logging
-    logger = logging.getLogger('IO')
-    logger.info("Reading %s"%dir)
-    xml_files= []
-    next =[]
-    for x in os.listdir(dir):
-        if(os.path.isdir("%s%s"%(dir,x))):
-            xml_files += ["%s%s/%s"%(dir,x,y) for y in os.listdir("%s%s/"%(dir,x)) if y.endswith('.xml')]
-            next  += read_jstor_data("%s%s/"%(dir,x))
-    return xml_files + next 
+
 def read_iob_files(inp_dir,extension=".iob"):
     import glob
     import os
@@ -544,6 +441,7 @@ def read_iob_files(inp_dir,extension=".iob"):
         instances +=temp
         logger.debug("read %i instances from file %s"%(len(temp),infile))
     return instances
+
 def scan_iob_files(inp_dir):
     import glob
     import os
@@ -556,6 +454,7 @@ def scan_iob_files(inp_dir):
         aph_number = re.match(exp,fname).groups()[0]
         result[fname] = aph_number
     return result
+
 def main():
     insts = read_IOB_file(sys.argv[1])
     tag_name = 'CRF'
@@ -565,5 +464,6 @@ def main():
     for i in res:
         print i
         print re.sub(r'[^\w]','',i)
+
 if __name__ == "__main__":
     main()
