8e50807e4dfabb8a00f1c5277fccc1abc1c4065f
mromanello <matteo.romanello@gmail.com>
2017-05-29 23:50:48 +0200

    integrated Matteo F's get_df_data(), and renamed to IO.load_brat_data

--- citation_extractor/Utils/IO.py
+++ citation_extractor/Utils/IO.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 # author: Matteo Romanello, matteo.romanello@gmail.com
 
-
+from __future__ import print_function
+import logging
 import os
 import codecs
 import glob
-import sys,pprint,re,string,logging
-#import citation_extractor
+import sys,pprint,re,string
+import pandas as pd
 from random import *
-import xml.dom.minidom as mdom
 from pyCTS import CTS_URN
+from .strmatching import StringUtils  
+
+#import citation_extractor
+#import xml.dom.minidom as mdom
 
 global logger
 logger = logging.getLogger(__name__)
+
+NIL_ENTITY = "urn:cts:GreekLatinLit:NIL"
 
 def read_ann_file(fileid,ann_dir):
     """
@@ -133,6 +139,8 @@ def annotations2references(doc_id, directory, kb):
             if(offset_start <= nl_offset[0] and offset_end <= nl_offset[0]):
                 return (n,newline_offsets[n-1][1],newline_offsets[n][0])
     
+    import knowledge_base
+
     entities, relations, disambiguations = read_ann_file_new(doc_id, directory)
     fulltext = codecs.open("%s%s%s"%(directory,doc_id,"-doc-1.txt"),"r","utf-8").read()
     newlines = find_newlines(fulltext)
@@ -345,3 +353,216 @@ def instance_to_string(instance):
     TODO
     """
     return ["\t".join(token_to_string(feature_set)) for feature_set in instance]
+
+def sort_mentions_by_appearance(entities, relations):
+    """
+    Return an ordered sequence of entity/relation IDs, in the same order as they appear in the document.
+
+    :param entities:
+        The dictionary of the entities in the document.
+    :param relations:
+        The dictionary of the relations in the document.
+    :return:
+        A list containing the ids in order.
+    """
+    entities_id_set = set(entities.keys())
+    by_offset = []
+
+    for k, v in relations.iteritems():
+        entity_id_0 = v['arguments'][0]
+        entity_id_1 = v['arguments'][1]
+        entities_id_set.discard(entity_id_0)
+        entities_id_set.discard(entity_id_1)
+        entity_0 = entities[entity_id_0]
+        entity_1 = entities[entity_id_1]
+        first_start = min(int(entity_0['offset_start']), int(entity_1['offset_start']))
+        second_start = max(int(entity_0['offset_start']), int(entity_1['offset_start']))
+        offset = float(str(first_start) + '.' + str(second_start))
+        t = (k, offset)
+        by_offset.append(t)
+
+    for eid in entities_id_set:
+        entity_0 = entities[eid]
+        offset = float(entity_0['offset_start'])
+        t = (eid, offset)
+        by_offset.append(t)
+
+    by_offset = [id for id, offset in sorted(by_offset, key=lambda (id, offset): offset)]
+    return by_offset
+
+def load_brat_data(extractor, knowledge_base, postaggers, aph_ann_files, aph_titles):
+    """
+    Utility function to load a set of APh documents (in brat format) and prepare them 
+    in a format suitable for processing (typically when carrying out the evaluation).
+
+    :param citation_extractor: instance of `core.citation_extractor`
+    :parm knowledge_base: instance of `knowledge_base.KnowledgeBase`
+    :param aph_ann_files: a tuple: [0] the base directory; [1] a list of file names
+    :param aph_titles: `pandas.DataFrame` with column 'title'
+    :return: a `pandas.DataFrame` (columns: 'surface', 'surface_norm', 'scope', 'type',
+        'other_mentions', 'prev_mentions', 'urn', 'urn_clean','doc_id', 'doc_title', 
+        'doc_title_mentions', 'doc_title_norm', 'doc_text')
+
+    """
+    from citation_extractor.pipeline import extract_entity_mentions
+
+    cols = ['surface', 'surface_norm', 'scope', 'type', 'other_mentions', 'prev_mentions', 'urn', 'urn_clean',
+            'doc_id', 'doc_title', 'doc_title_mentions', 'doc_title_norm', 'doc_text']
+    df_data = pd.DataFrame(dtype='object', columns=cols)
+
+    # Read all annotated files
+    ann_dir, files = aph_ann_files
+    for filename in files:
+        if filename.endswith('.ann'):
+            logger.debug('Reading file: {}'.format(filename))
+
+            # Read doc annotations
+            file_suffix = filename.replace('-doc-1.ann', '')
+            entities, relations, disambiguations = read_ann_file_new(file_suffix, ann_dir + '/')
+
+            # Read doc text
+            doc_text = None
+            filename_text = filename.replace('.ann', '.txt')
+            with open(os.path.join(ann_dir, filename_text)) as f:
+                doc_text = f.read()
+                doc_text = unicode(doc_text, 'utf-8')
+            logger.debug(u'Document text: {}'.format(doc_text))
+
+            # Get title
+            doc_title = None
+            file_id = file_suffix.replace('.txt', '')
+            if file_id in aph_titles.index:
+                doc_title = aph_titles.loc[file_id, 'title']
+                doc_title = unicode(doc_title, 'utf-8')
+            logger.debug(u'Document title: {}'.format(doc_title))
+            
+            try:
+                # Extract mentions from the title, list of (type, surface) tuples
+                doc_title_extracted_mentions = extract_entity_mentions(doc_title, extractor, postaggers, norm=True)
+            except Exception, e:
+                doc_title_extracted_mentions = []
+                print(e)
+                print(doc_title)
+                print(file_id)
+            
+            # Normalize title
+            doc_title_norm = StringUtils.normalize(doc_title)
+
+            # Order the appearance of the mentions in the doc
+            ordered_mentions = sort_mentions_by_appearance(entities, relations)
+            logger.debug('Mentions appearance: {}'.format(ordered_mentions))
+
+            # Rearrange disambiguations
+            disambiguations_new = dict(map(lambda e: (e['anchor'], e['text']), disambiguations))
+            
+            prev_entities = []
+            for mention_id in ordered_mentions:
+
+                mention_data_id = file_id + '-' + mention_id
+                mention_urn = NIL_ENTITY
+                clean_urn = mention_urn
+                mention_surface = None
+                mention_scope = None
+                mention_type = None
+
+                # It's a relation
+                if mention_id.startswith('R'):
+                    relation = relations[mention_id]
+
+                    # Unpack the relation
+                    entity_0 = entities[relation['arguments'][0]]
+                    entity_1 = entities[relation['arguments'][1]]
+
+                    # Sanity check for types of relation members
+                    no_refscope = ['AAUTHOR', 'AWORK', 'REFAUWORK']
+                    if entity_0['entity_type'] in no_refscope and entity_1['entity_type'] == 'REFSCOPE':
+                        pass
+
+                    elif entity_1['entity_type'] in no_refscope and entity_0['entity_type'] == 'REFSCOPE':
+                        logger.warning('Swapped entities in relation {} in doc {}'.format(mention_id, filename))
+                        entity_0 = entities[relation['arguments'][1]]
+                        entity_1 = entities[relation['arguments'][0]]
+
+                    else:
+                        logger.error('Unknown types in relation {} in doc {}'.format(mention_id, filename))
+                        continue
+
+                    # Update fields
+                    if mention_id in disambiguations_new:
+                        mention_urn = disambiguations_new[mention_id]
+                    mention_surface = entity_0['surface']
+                    mention_scope = entity_1['surface']
+                    mention_type = entity_0['entity_type']
+
+                # It's a non-relation
+                elif mention_id.startswith('T'):
+                    entity = entities[mention_id]
+
+                    # Avoid to disambiguate the mention if it's a REFSCOPE (alone)
+                    if entity['entity_type'] == 'REFSCOPE':
+                        logger.warning('Lonely REFSCOPE with id: {} in doc: {}'.format(mention_id, filename))
+                        continue
+
+                    # Update fields
+                    if mention_id in disambiguations_new:
+                        mention_urn = disambiguations_new[mention_id]
+                    mention_surface = entity['surface']
+                    mention_type = entity['entity_type']
+
+                else:
+                    logger.error('Unknown mention id: {} in doc {}'.format(mention_id, filename))
+                    continue
+                    
+                # Get clean URN (without passage), skip if non-valid
+                if mention_urn != NIL_ENTITY:
+                    try:
+                        cts_urn = CTS_URN(mention_urn)
+                        clean_urn = cts_urn.get_urn_without_passage()
+                        knowledge_base.get_resource_by_urn(clean_urn)
+                    except Exception, e:
+                        logger.error(e)
+                        logger.warning('Failed parsing the URN: |{}| at: {}'.format(mention_urn, file_id))
+                        continue
+                        
+                # Keep track of previous mentions
+                mention_prev_entities = list(prev_entities) # copy
+                prev_entities.append(mention_data_id)
+                
+                df_data.loc[mention_data_id, 'surface'] = mention_surface
+                df_data.loc[mention_data_id, 'surface_norm'] = StringUtils.normalize(mention_surface)
+                df_data.loc[mention_data_id, 'scope'] = mention_scope
+                df_data.loc[mention_data_id, 'type'] = mention_type
+                df_data.loc[mention_data_id, 'prev_mentions'] = mention_prev_entities
+                df_data.loc[mention_data_id, 'doc_id'] = file_id
+                df_data.loc[mention_data_id, 'doc_title'] = doc_title
+                df_data.loc[mention_data_id, 'doc_title_mentions'] = doc_title_extracted_mentions
+                df_data.loc[mention_data_id, 'doc_title_norm'] = doc_title_norm
+                df_data.loc[mention_data_id, 'doc_text'] = doc_text
+                df_data.loc[mention_data_id, 'urn'] = mention_urn
+                df_data.loc[mention_data_id, 'urn_clean'] = clean_urn
+                
+            # Add successfully parsed mentions of the doc to other_mentions field of each mention of the doc
+            for m_id in prev_entities:
+                other_mentions = list(prev_entities)
+                other_mentions.remove(m_id)
+                df_data.loc[m_id, 'other_mentions'] = other_mentions
+
+    nb_total = df_data.shape[0]
+    nb_authors = df_data[df_data['type'] == 'AAUTHOR'].shape[0]
+    nb_works = df_data[df_data['type'] == 'AWORK'].shape[0]
+    nb_refauworks = df_data[df_data['type'] == 'REFAUWORK'].shape[0]
+    nb_nil = df_data[df_data['urn'] == NIL_ENTITY].shape[0]
+
+    logger.info('DATA - Total: {}, AAUTHORS: {} ({:.1f}%), AWORK: {} ({:.1f}%), REFAUWORK: {} ({:.1f}%), NIL: {} ({:.1f}%)'.format(
+        nb_total,
+        nb_authors,
+        float(nb_authors) / max(nb_total, 1) * 100,
+        nb_works,
+        float(nb_works) / max(nb_total, 1) * 100,
+        nb_refauworks,
+        float(nb_refauworks) / max(nb_total, 1) * 100,
+        nb_nil,
+        float(nb_nil) / max(nb_total, 1) * 100
+    ))
+
+    return df_data
