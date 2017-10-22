663111e2d49d585e3ba88b7cad044faf8c6dad10
Matteo Filipponi <matteo.filipponi@epfl.ch>
2017-10-22 20:37:34 +0200

    Add tfifd function

--- citation_extractor/ned/ml_matchers.py
+++ citation_extractor/ned/ml_matchers.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import logging
+import pkg_resources
+import os
+from stop_words import safe_get_stop_words
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 LOGGER = logging.getLogger(__name__)
 
@@ -17,16 +22,19 @@ REFAUWORK_TYPE = 'REFAUWORK'
 
 
 class MLCitationMatcher(object):
-    def __init__(self, kb):
+    def __init__(self, kb=None):
         LOGGER.info('Initializing Citation Matcher')
         # TODO: Load, pre-process, ... the KB (?)
         self._kb = None
         self._feature_extractor = None
         self._ranker = None
 
-    def train(self, train_data, wikipages_dir, include_nil=True):
+    def train(self, train_data=None, wikipages_dir=None, include_nil=True):
         LOGGER.info('Starting training')
         # TODO: get tf-idf data from wiki texts
+        tfidf = self._compute_tfidf_matrix()
+        for k, v in  tfidf.iteritems():
+            print(k, v)
         # TODO: compute probs from train data
         # TODO: generate features for candidates (FeatureExtractor)
         # TODO: generate ranking function (SVMRank)
@@ -38,9 +46,62 @@ class MLCitationMatcher(object):
         # TODO: rank candidates (SVMRank)
         return None
 
+    # TODO: should be in FeatureExtractor
+    def _compute_tfidf_matrix(self, base_dir=None):
+        LOGGER.info('Computing TF-IDF matrix (base_dir={})'.format(base_dir))
+        tfidf_data = {}
+
+        # Compute tf-idf distribution for each language
+        for lang in LANGUAGES:
+            lang_data = {}
+
+            if not base_dir:
+                resources_dir = 'data/wikipages/text/authors/{}'.format(lang)
+                text_authors_dir_lang = pkg_resources.resource_filename('citation_extractor', resources_dir)
+                text_authors_files = pkg_resources.resource_listdir('citation_extractor', resources_dir)
+            else:
+                text_authors_dir_lang = os.path.join(base_dir, lang)
+                text_authors_files = os.listdir(text_authors_dir_lang)
+
+            texts = []
+            urn_to_index = {}
+            index = 0
+            for file in text_authors_files:
+                if not file.endswith('.txt'):
+                    continue
+
+                urn = file.replace('.txt', '')
+                filepath = os.path.join(text_authors_dir_lang, file)
+                with open(filepath) as txt_file:
+                    text = txt_file.read()
+                texts.append(text)
+                urn_to_index[urn] = index
+                index += 1
+
+            # Dictionary mapping a URN to an index (row)
+            lang_data['urn_to_index'] = urn_to_index
+
+            tfidf_vectorizer = TfidfVectorizer(
+                input='content',
+                strip_accents='unicode',
+                analyzer='word',
+                stop_words=safe_get_stop_words(lang)
+            )
+
+            # Language-specific vectorizer
+            lang_data['vectorizer'] = tfidf_vectorizer
+
+            # Tf-idf matrix computed with the specific vectorizer
+            tfidf_matrix = tfidf_vectorizer.fit_transform(texts)
+            lang_data['matrix'] = tfidf_matrix
+
+            tfidf_data[lang] = lang_data
+
+        return tfidf_data
+
 
 class FeatureExtractor(object):
-    def __init__(self, tfidf_data, entities_prior_prob, m_given_e_prob, e_given_m_prob):
+    def __init__(self, tfidf_data=None, entities_prior_prob=None, m_given_e_prob=None, e_given_m_prob=None):
         LOGGER.info('Initializing Feature Extractor')
         self.tfidf = tfidf_data
         self.prior_prob = entities_prior_prob
@@ -74,3 +135,15 @@ class SVMRank(object):
         LOGGER.info('Predicting ...')
         # TODO: apply dot product + sort
         return None
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    matcher = MLCitationMatcher()
+    matcher.train()
+
+
+
+if __name__ == '__main__':
+    main()
