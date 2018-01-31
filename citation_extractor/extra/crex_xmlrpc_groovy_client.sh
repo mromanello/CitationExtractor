84a524080cbe6c85751bb21eff84a55a2d7a0c84
mromanello <matteo.romanello@gmail.com>
2018-01-31 14:52:45 +0100

    minor changes mostly related to ditching CRF++ for crfsuite+sklearn

--- citation_extractor/extra/crex_xmlrpc_groovy_client.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-import groovy.net.xmlrpc.*
-
-def server = new XMLRPCServerProxy("http://137.73.122.221:8000/rpc/crex")
-
-def result = server.version() + server.test()
-
-println result
