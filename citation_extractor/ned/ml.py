cfb2ba9d7e763337b5e332254198a694ac438712
mromanello <matteo.romanello@gmail.com>
2018-03-19 11:19:53 +0100

    partly parallelized _select_C_best (with dask)

--- citation_extractor/ned/ml.py
+++ citation_extractor/ned/ml.py
@@ -7,12 +7,14 @@ from __future__ import print_function
 
 import logging
 import time
+import pdb
 
 import numpy as np
+from dask import delayed, compute
+from dask.distributed import Client, progress
 
 from citation_extractor.Utils.extra import avg
-from sklearn import (cross_validation, linear_model, model_selection,
-                     preprocessing, svm)
+from sklearn import (model_selection, preprocessing, svm)
 from sklearn.feature_extraction import DictVectorizer
 
 LOGGER = logging.getLogger(__name__)
@@ -32,6 +34,46 @@ class LinearSVMRank(object):
         self._classifier = classifier
         self._dv = DictVectorizer(sparse=False)
         # TODO: add feature to use sparse (MF)
+
+    @staticmethod
+    def pairwise_transformation(X, y, groups):
+        """Apply the pairwise transformation to groups of labeled vectors.
+
+        :param X: the matrix describing the vectors
+        :type X: numpy.ndarray
+        :param y: the labels of the vectors
+        :type y: numpy.ndarray
+        :param groups: the labels of the groups
+        :type groups: numpy.ndarray
+
+        :return: the pairwise-transformed vectors with their labels
+        :rtype: numpy.ndarray, numpy.ndarray
+        """
+        LOGGER.debug('Applying pairwise transformation')
+        nb_groups = len(set(groups))
+        Xp, yp = [], []
+        k = 0
+        for i in set(groups):
+            group_idx = (groups == i)
+            X_group = X[group_idx]
+            y_group = y[group_idx]
+
+            y_group_true_idx = (y_group == 1)
+            X_group_true = X_group[y_group_true_idx][0]
+            X_group_false = X_group[~y_group_true_idx]
+
+            for x in X_group_false:
+                Xp.append(X_group_true - x)
+                yp.append(1)
+
+                # output balanced classes
+                if yp[-1] != (-1) ** k:
+                    yp[-1] *= -1
+                    Xp[-1] *= -1
+                k += 1
+
+        Xp, yp = map(np.asanyarray, (Xp, yp))
+        return Xp, yp
 
     def _pairwise_transformation(self, X, y, groups):
         """Apply the pairwise transformation to groups of labeled vectors.
@@ -89,12 +131,65 @@ class LinearSVMRank(object):
         :return: The best C parameter for SVM
         :rtype: float
         """
-        LOGGER.info(
-            'Selecting best C prameter using k-fold cross\
-             validation (k={}, cache_size={})'.format(k, cache_size)
-        )
-        C_scores = []
 
+        def _test_C_value(C, k, X, y, groups):
+            gkf = model_selection.GroupKFold(n_splits=k)
+            LOGGER.info("Testing C={} (k={})".format(C, k))
+            tasks = [
+                _do_split(C, train, test, X, y, groups)
+                for train, test in gkf.split(X, y, groups)
+            ]
+            scores = compute(*tasks)
+            avg_score = avg(scores)
+            LOGGER.info("Done testing C={}: avg score={}".format(C, avg_score))
+            return (C, avg_score)
+
+        @delayed
+        def _do_split(C, split_train, split_test, X, y, groups):
+            # Fit the model
+            classifier = svm.SVC(
+                kernel='linear',
+                C=C,
+                cache_size=100
+            )
+            X_train, X_test = X[split_train], X[split_test]
+            y_train, y_test = y[split_train], y[split_test]
+            groups_train, groups_test = groups[split_train], groups[split_test]
+            Xp, yp = LinearSVMRank.pairwise_transformation(
+                X_train,
+                y_train,
+                groups_train
+            )
+            Xp_norm = preprocessing.normalize(Xp)
+            LOGGER.info("Fitting classifier... (C={})".format(C))
+            start = time.clock()
+            classifier.fit(Xp_norm, yp)
+            end = time.clock()
+            LOGGER.info("Done fitting, took {} secs".format(end - start))
+
+            test_score = 0
+            nb_groups = len(set(groups_test))
+            nb_correct = 0
+
+            # Predict for each group
+            for i in set(groups_test):
+                group_idx = (groups_test == i)
+                X_group = X_test[group_idx]
+                y_group = y_test[group_idx]
+
+                coef = classifier.coef_.ravel()
+                norm_coef = coef / np.linalg.norm(coef)
+                rank_scores = np.dot(X_group, norm_coef.T).ravel().tolist()
+                sorted_columns = np.argsort(rank_scores)[::-1].tolist()
+                is_correct = y_group[sorted_columns[0]] == 1
+
+                if is_correct:
+                    nb_correct += 1
+
+            test_score = nb_correct / nb_groups
+            return test_score
+
+        # adjust number of folds (k) based on number of groups (instances)
         total_nb_groups = len(set(groups))
         if total_nb_groups < k:
             LOGGER.warning(
@@ -103,63 +198,15 @@ class LinearSVMRank(object):
             )
             k = total_nb_groups
 
-        for C in 10. ** np.arange(-3, 3):
-            split_n = 0
-            gkf = model_selection.GroupKFold(n_splits=k)
-            scores = []
+        LOGGER.info(
+            'Selecting best C prameter using k-fold cross\
+             validation (k={}, cache_size={})'.format(k, cache_size)
+        )
 
-            for train, test in gkf.split(X, y, groups):
-                split_n += 1
-                LOGGER.info(
-                    "Testing C param. {} (split {})".format(C, split_n)
-                )
-                X_train, X_test = X[train], X[test]
-                y_train, y_test = y[train], y[test]
-                groups_train, groups_test = groups[train], groups[test]
-
-                # Fit the model
-                classifier = svm.SVC(
-                    kernel='linear',
-                    C=C,
-                    cache_size=cache_size
-                )
-                Xp, yp = self._pairwise_transformation(
-                    X_train,
-                    y_train,
-                    groups_train
-                )
-                Xp_norm = preprocessing.normalize(Xp)
-                LOGGER.info("Fitting classifier...")
-                start = time.clock()
-                classifier.fit(Xp_norm, yp)
-                end = time.clock()
-                LOGGER.info("Done fitting, took {} secs".format(end - start))
-
-                test_score = 0
-                nb_groups = len(set(groups_test))
-                nb_correct = 0
-
-                # Predict for each group
-                for i in set(groups_test):
-                    group_idx = (groups_test == i)
-                    X_group = X_test[group_idx]
-                    y_group = y_test[group_idx]
-
-                    coef = classifier.coef_.ravel()
-                    norm_coef = coef / np.linalg.norm(coef)
-                    rank_scores = np.dot(X_group, norm_coef.T).ravel().tolist()
-                    sorted_columns = np.argsort(rank_scores)[::-1].tolist()
-                    is_correct = y_group[sorted_columns[0]] == 1
-
-                    if is_correct:
-                        nb_correct += 1
-
-                test_score = nb_correct / nb_groups
-                scores.append(test_score)
-
-            avg_score = avg(scores)
-            C_scores.append((C, avg_score))
-
+        C_scores = [
+            _test_C_value(C, k, X, y, groups)
+            for C in 10. ** np.arange(-3, 3)
+        ]
         C_scores.sort(key=lambda tup: tup[1], reverse=True)
         best_C = C_scores[0][0]
         best_C_score = C_scores[0][1]
