548f9123f9a98dc9b7c7ade0549268d0803fefa7
Matteo Romanello <matteo.romanello@gmail.com>
2019-11-05 14:44:04 +0100

    Herr Schmidt changed path for TT data online, fixed the install script

--- install_treetagger.sh
+++ install_treetagger.sh
@@ -8,22 +8,22 @@ wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tree-tagger-li
 wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tagger-scripts.tar.gz
 wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/install-tagger.sh
 # parameter files for tagger
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/dutch-par-linux-3.2-utf8.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-par-linux-3.2-utf8.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/french-par-linux-3.2-utf8.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/german-par-linux-3.2-utf8.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/italian-par-linux-3.2-utf8.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/latin-par-linux-3.2.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/portuguese-par-linux-3.2-utf8.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish-par-linux-3.2-utf8.bin.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/dutch.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/french.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/german.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/italian.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/latin.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/portuguese.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish.par.gz
 # parameter files for chunker
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-chunker-par-linux-3.2-utf8.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/french-chunker-par-linux-3.2-utf8.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/german-chunker-par-linux-3.2-utf8.bin.gz
-wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish-chunker-par-linux-3.2-utf8.bin.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-chunker.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/french-chunker.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/german-chunker.par.gz
+wget https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish-chunker.par.gz
 # let's run the installation script
-chmod +x install-tagger.sh 
+chmod +x install-tagger.sh
 chown $USER:$USER -R .
-chmod 777 -R . 
+chmod 777 -R .
 ./install-tagger.sh
 rm *.gz
